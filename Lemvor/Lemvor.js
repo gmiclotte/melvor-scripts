63ab5a81de8aa10442d2c62b34a2ffcd853cee23
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-03-22 21:56:49 +0100

    Lemvor: v0.0.10

--- Lemvor/Lemvor.js
+++ Lemvor/Lemvor.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Lemvor
-// @version      0.0.9
+// @version      0.0.10
 // @namespace    github.com/gmiclotte
 // @description  lemon
 // @author       GMiclotte
