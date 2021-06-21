d2a08fd6908a1ce553bcc2bbd36de92e658d23ff
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-06-21 23:33:54 +0200

    Show Modifiers v0.0.15

--- Show-Modifiers/Show-Modifiers.js
+++ Show-Modifiers/Show-Modifiers.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Melvor Show Modifiers
-// @version      0.0.14
+// @version      0.0.15
 // @description  Adds a button to show all your modifiers
 // @author       GMiclotte
 // @match        https://*.melvoridle.com/*
