010d793b71d594b7ba2fa384c6d3fb85a1351f59
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-06-30 14:29:30 +0200

    Loot Container: v0.0.2

--- Loot-Container/Loot-Container.js
+++ Loot-Container/Loot-Container.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Melvor Loot Container
 // @namespace   github.com/gmiclotte
-// @version     0.0.1
+// @version     0.0.2
 // @description For monster drops and pickpocketing: shows drop rates, avg drops per kill, and current number owned.
 // @author      GMiclotte
 // @match       https://*.melvoridle.com/*
