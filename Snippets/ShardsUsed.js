e1bfcde1729ac8c4d743b50d06ceeefd08846af7
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-08-09 23:28:05 +0200

    Snippets: compute gp spent on used summoning shards, v0.21

---
+++ Snippets/ShardsUsed.js
@@ -0,0 +1,6 @@
+// compute amount of gp spent on summoning shards that have been used (for summoning or agility obstacles)
+items.map((x, i) => [x, i])
+    .filter(x => x[0].type === 'Shard' && x[0].category === 'Summoning')
+    .map(x => x[1])
+    .map(x => (itemStats[x].stats[0] - getBankQty(x) - itemStats[x].stats[1]) * items[x].buysFor)
+    .reduce((a, b) => a + b, 0);
