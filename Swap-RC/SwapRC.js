5e8993330716b97a89e090f68d872d21cd7cde5b
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-08-09 23:26:09 +0200

    Swap RC: v0.0.2

--- Swap-RC/SwapRC.js
+++ Swap-RC/SwapRC.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Melvor Swap RC
-// @version      0.0.1
+// @version      0.0.2
 // @description  Automatically changes RC runes.
 // @author		 GMiclotte
 // @match        https://*.melvoridle.com/*
