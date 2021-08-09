c193b9f8ef40575a617ea6b326b59e713f4d8229
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-08-09 23:28:42 +0200

    Snippets: list Summoning synergies combat and non-combat, v0.21

---
+++ Snippets/PrintSynergyList.js
@@ -0,0 +1,22 @@
+printSynergy = (x, y) => console.log('- [ ]',
+    x.summoningID,
+    parseInt(y),
+    items[x.itemID].name,
+    items[summoningItems[y].itemID].name,
+    SUMMONING.Synergies[x.summoningID][y].description,
+    SUMMONING.Synergies[x.summoningID][y].modifiers
+);
+
+// get combat synergies
+summoningItems.filter(x => items[x.itemID].summoningMaxHit).map(x => {
+    for (y in SUMMONING.Synergies[x.summoningID]) {
+        printSynergy(x, y);
+    }
+});
+
+// get non-combat synergies
+summoningItems.filter(x => !items[x.itemID].summoningMaxHit).map(x => {
+    for (y in SUMMONING.Synergies[x.summoningID]) {
+        printSynergy(x, y);
+    }
+});
