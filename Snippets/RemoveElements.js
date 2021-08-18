25ecd23752736767ceebcea30cc7b05a47cc7149
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-08-18 13:29:33 +0200

    Snippets: Remove Elements

---
+++ Snippets/RemoveElements.js
@@ -0,0 +1,25 @@
+
+///////////////////
+//remove elements//
+///////////////////
+// combat
+document.getElementById('offline-combat-alert').remove();
+
+// summoning marks
+// green
+document.getElementById('summoning-category-0').children[0].children[0].children[2].remove();
+// orange and red
+document.getElementById('summoning-category-0').children[0].children[0].children[1].remove();
+
+// summoning tablets
+document.getElementById('summoning-notice').remove();
+
+// alt. magic
+document.getElementById('magic-container').children[0].children[1].remove();
+
+// cloud saving
+document.getElementById('header-cloud-save-time').remove();
+document.getElementById('header-cloud-save-btn-connected').remove();
+
+// minibar-max-cape
+document.getElementById('minibar-max-cape').remove();
