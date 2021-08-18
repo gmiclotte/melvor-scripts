e19522b280a9f6773290ca8053d3116d8108e553
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-08-18 13:30:59 +0200

    Snippets: Spawn Ahrenia

---
+++ Snippets/SpawnAhrenia.js
@@ -0,0 +1,29 @@
+
+/////////////////
+//spawn Ahrenia//
+/////////////////
+window.spawnAhrenia = (phaseToSpawn = 1) => {
+    // run
+    combatManager.runCombat();
+    // set respawn to 0
+    if (!petUnlocked[0]) {
+        unlockPet(0);
+    }
+    PETS[0].modifiers.decreasedMonsterRespawnTimer = 0;
+    player.computeAllStats();
+    PETS[0].modifiers.decreasedMonsterRespawnTimer = 3000 - TICK_INTERVAL - player.modifiers.decreasedMonsterRespawnTimer + player.modifiers.increasedMonsterRespawnTimer;
+    player.computeAllStats();
+    // unlock itm
+    dungeonCompleteCount[CONSTANTS.dungeon.Fire_God_Dungeon] = Math.max(
+        dungeonCompleteCount[CONSTANTS.dungeon.Fire_God_Dungeon],
+        1,
+    );
+    skillLevel[CONSTANTS.skill.Slayer] = Math.max(
+        skillLevel[CONSTANTS.skill.Slayer],
+        90,
+    );
+    // skip to desired phase
+    combatManager.selectDungeon(15);
+    combatManager.dungeonProgress = 19 + phaseToSpawn;
+    combatManager.loadNextEnemy();
+}
