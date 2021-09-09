82735b82f4f080ff1060f0ae5e52bc372d5fc390
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-09-09 17:15:38 +0200

    Snippets: enable wrapped snippets

--- Snippets/combine.sh
+++ Snippets/combine.sh
@@ -1,15 +1,16 @@
 cat Wrapper.js.header > ActiveSnippets.js
-# cat AgilityObstacleBuildsRemaining.js >> ActiveSnippets.js
+cat AgilityObstacleBuildsRemaining.js >> ActiveSnippets.js
+cat DefensePure.js >> ActiveSnippets.js
 cat MasteryBars.js >> ActiveSnippets.js
 cat MasteryBuyer.js >> ActiveSnippets.js
-# cat PrintSynergyList.js >> ActiveSnippets.js
+cat PrintSynergyList.js >> ActiveSnippets.js
 cat QuickEquipCape.js >> ActiveSnippets.js
 cat RemoveElements.js >> ActiveSnippets.js
 cat RerollSlayer.js >> ActiveSnippets.js
 cat RockOrder.js >> ActiveSnippets.js
 cat ShardsUsed.js >> ActiveSnippets.js
 cat ShowCookingFish.js >> ActiveSnippets.js
-# cat SpawnAhrenia.js >> ActiveSnippets.js
+cat SpawnAhrenia.js >> ActiveSnippets.js
 cat UnlimitedPool.js >> ActiveSnippets.js
 cat Unsell.js >> ActiveSnippets.js
 cat Wrapper.js.footer >> ActiveSnippets.js
