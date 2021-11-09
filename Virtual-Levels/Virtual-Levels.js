bb985755596f34e974fcea1d0ba280d129a85c68
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-11-09 21:36:31 +0100

    Virtual Levels: v0.1.3

--- Virtual-Levels/Virtual-Levels.js
+++ Virtual-Levels/Virtual-Levels.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Melvor Virtual Levels
-// @version		 0.1.2
+// @version		 0.1.3
 // @namespace    github.com/gmiclotte
 // @author       GMiclotte
 // @match        https://*.melvoridle.com/*
@@ -156,7 +156,7 @@ function script() {
     }
 
     function loadScript() {
-        if (confirmedLoaded) {
+        if (typeof confirmedLoaded !== undefined && confirmedLoaded) {
             // Only load script after game has opened
             clearInterval(scriptLoader);
             injectScript(script);
