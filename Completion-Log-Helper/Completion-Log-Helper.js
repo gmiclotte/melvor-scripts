9e5de1bd526a1eeb0e5128957f8c439b091e9415
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-12-23 20:33:29 +0100

    Completion Log Helper: update for 1.0

--- Completion-Log-Helper/Completion-Log-Helper.js
+++ Completion-Log-Helper/Completion-Log-Helper.js
@@ -2,8 +2,7 @@
 // @name        Melvor Completion Log Helper
 // @namespace   http://tampermonkey.net/
 // @version     0.6.2
-// @description Replaces question mark icon in the Item Completion Log, Mastery Progression, Pet Completion and Monster Completion Log with greyed out images of items/pets/monsters that you have yet to get/kill and adds links to the wiki. Also includes toggle for highlighting items in bank.
-// @author      Breindahl#2660
+// @description The completion logs now show images of undiscovered items/monsters/pets, including those entities that do not count towards completion.
 // @author		GMiclotte
 // @include		https://melvoridle.com/*
 // @include		https://*.melvoridle.com/*
@@ -17,6 +16,7 @@
 // ==/UserScript==
 /* jshint esversion: 6 */
 
+// Thanks to Breindahl#2660 for the original implementation
 // Big thanks to Visua#9999 for helping with parts of the code and troubleshooting
 
 ((main) => {
@@ -25,122 +25,45 @@
     document.body.appendChild(script).parentNode.removeChild(script);
 })(() => {
 
+    function patchCode(code, additionalPatch) {
+        code = code.toString();
+        code = code.replace(/!items\[[a-zA-Z]*\].ignoreCompletion/g, 'true');
+        code = code.replace(/!MONSTERS\[[a-zA-Z]*\].ignoreCompletion/g, 'true');
+        code = code.replace(/!PETS\[[a-zA-Z]*\].ignoreCompletion/g, 'true');
+        if (additionalPatch !== undefined) {
+            code = additionalPatch(code);
+        }
+        return code.replace(/^function (\w+)/, 'window.$1 = function');
+    }
+
+    const toPatch = [
+        filterItemLog,
+        createItemLogTooltip,
+        createMonsterLogTooltip,
+        createPetLogTooltip,
+    ];
+
+    function additionalPatchApplyCompletionLogEntryElement(code) {
+        code = code.split(/Element\(-8,/);
+        for (let i = 1; i < code.length; i++) {
+            code[i] = code[i].replace(';', `; const htmlIdx = html.lastIndexOf('<img ') + 5; html = html.substring(0, htmlIdx) + 'style="opacity: 25%"' + html.substring(htmlIdx); `);
+        }
+        return code.join('Element(id,');
+    }
+
     function startCompletionLogHelper() {
-        // Loading script
+        // patch functions
+        toPatch.forEach(x => eval(patchCode(x)));
+        eval(patchCode(applyCompletionLogEntryElement, additionalPatchApplyCompletionLogEntryElement));
 
-        // item log
-        $('#modal-item-log').find('.block-options').prepend('<button id="toggleItems" class="btn btn-sm btn-success" style="margin:0px 10px; float:left;">Toggle Found</button>');
-        $('#modal-item-log').find('.block-options').prepend('<img class="skill-icon-xs m-1 BankBordersOn pointer-enabled" id="toggleBankBorders" style="opacity:50%" src="assets/media/main/bank_header.svg">');
-        $(document).ready(function () {
-            $("#toggleItems").click(function () {
-                $(".item-found").toggle();
-            });
-            $("#toggleBankBorders").click(function () {
-                if ($("#toggleBankBorders").hasClass("BankBordersOff")) {
-                    $("#toggleBankBorders").removeClass("BankBordersOff").addClass("BankBordersOn").css('opacity', '100%');
-                    document.getElementsByClassName('item-in-bank').forEach(x => x.style.border = '1px gold dotted');
-                } else {
-                    $("#toggleBankBorders").removeClass("BankBordersOn").addClass("BankBordersOff").css('opacity', '50%');
-                    document.getElementsByClassName('item-in-bank').forEach(x => x.style.border = '');
-                }
-            });
-        });
+        // reset loaded variables
+        itemLogLoaded = false;
+        monsterLogLoaded = false;
+        petLogLoaded = false;
 
-        $('#modal-item-log').find('.block-options').prepend('<div class="input-group" style="float:left;width:auto"><input type="text" class="form-control text-danger btn btn-sm" id="searchTextboxItemLog" style="height:auto; text-align:left;" name="searchTextboxItemLog" placeholder="Search Item Log..." autocomplete="off"><div class="input-group-append"><button type="button" class="btn btn-danger btn-sm" id="clearItemLog">X</button></div></div>');
-
-        const itemIds = items.map((_, id) => id);
-
-        function updateItemLogSearch(search) {
-            if (search === '') {
-                $('#itemlog-container img').removeClass('d-none');
-                return;
-            }
-            search = search.toLowerCase();
-            const itemsResults = itemIds.filter(id => items[id].name.toLowerCase().includes(search) || items[id].category.toLowerCase().includes(search) || items[id].type.toLowerCase().includes(search) || id === Number(search));
-            $('#itemlog-container img').addClass('d-none');
-            for (let i = 0; i < itemsResults.length; i++) {
-                document.getElementById('item-log-img-' + itemsResults[i]).classList.remove('d-none');
-            }
-        }
-
-        function clearItemLogSearch() {
-            $('#searchTextboxItemLog').val('');
-            updateItemLogSearch($('#searchTextboxItemLog').val());
-        }
-
-        $("#clearItemLog").click(function () {
-            clearItemLogSearch();
-        });
-
-        $(document).ready(function () {
-            $("#searchTextboxItemLog").on("keyup", function () {
-                updateItemLogSearch($(this).val());
-            });
-        });
-
-        eval(
-            openItemLog.toString()
-                .replace(
-                    'function openItemLog',
-                    'openItemLog = function',
-                ).replace(
-                '$("#itemlog-container").html("");',
-                '$("#itemlog-container").html("");$("#toggleBankBorders").removeClass("BankBordersOn").addClass("BankBordersOff").css(\'opacity\', \'50%\');clearItemLogSearch();',
-            ).replace(
-                '" id="item-log-img-',
-                ' item-found" id="item-log-img-',
-            ).replace(
-                '{if(!items[i].ignoreCompletion){$("#itemlog-container").append(\'<img class="skill-icon-sm" id="item-log-img-\'+i+\'" src="\'+CDNDIR+\'assets/media/main/question.svg">\');itemTooltip="<div class=\'text-center\'>???</div>";}}',
-                '{ignoreCompletion = "";if (items[i].ignoreCompletion) {ignoreCompletion = "<br><span class=\'text-danger\'>Item does not count towards completion.</span>";}$("#itemlog-container").append(\'<a href="https://wiki.melvoridle.com/index.php?title=\' + items[i].name + \'" target="blank"><img class="skill-icon-sm" id="item-log-img-\' + i + \'" style="opacity:0.3;filter: grayscale(30%);" src="\' + getItemMedia(i) + \'"></a>\');itemTooltip = "<div class=\'text-center\'>" + items[i].name + ignoreCompletion + "</div>";}let haveInBank = getBankId(i);if (haveInBank !== -1) {$(\'#item-log-img-\' + i).addClass("item-in-bank");}',
-            )
-        );
-
-        // monster log
-        $('#modal-monster-log').find('.block-options').prepend('<button id="toggleMonsters" class="btn btn-sm btn-success">Toggle Found</button>');
-        $(document).ready(function () {
-            $("#toggleMonsters").click(function () {
-                $(".monster-found").toggle();
-            });
-        });
-
-        eval(
-            openMonsterLog.toString()
-                .replace(
-                    'function openMonsterLog',
-                    'openMonsterLog = function',
-                ).replace(
-                'if(monsterStats[i].stats[2]>0&&!MONSTERS[i].ignoreCompletion)',
-                'const ignoreCompletion = MONSTERS[i].ignoreCompletion ? "<br><span class=\'text-danger\'>Monster does not count towards completion.</span>" : ""; if (monsterStats[i].stats[2] > 0)',
-            ).replace(
-                '" id="monster-log-img',
-                ' monster-found" id="monster-log-img',
-            ).replace(
-                'if(!MONSTERS[i].ignoreCompletion){$("#monsterlog-container").append(\'<img class="skill-icon-md js-tooltip-enable" src="\'+CDNDIR+\'assets/media/main/question.svg" data-toggle="tooltip" data-html="true" data-placement="bottom" title="" data-original-title="???">\');monsterTooltip="<div class=\'text-center\'>???</div>";}',
-                '{$(\'#monsterlog-container\').append(\'<a href="https://wiki.melvoridle.com/index.php?title=\' + MONSTERS[i].name + \'" target="blank"><img class="skill-icon-md" id="monster-log-img-\' + i + \'" style="opacity:0.3;filter: grayscale(30%);" src="\' + MONSTERS[i].media + \'"></a>\');monsterTooltip = "<div class=\'text-center\'>" + MONSTERS[i].name + ignoreCompletion + "</div>";}',
-            )
-        );
-
-        // pet log
-        $('#modal-pet-log').find('.block-options').prepend('<button id="togglePets" class="btn btn-sm btn-success">Toggle Found</button>');
-        $(document).ready(function () {
-            $("#togglePets").click(function () {
-                $(".pet-found").toggle();
-            });
-        });
-
-        eval(
-            openPetLog.toString()
-                .replace(
-                    'function openPetLog',
-                    'openPetLog = function',
-                ).replace(
-                '" id="pet-log-img-',
-                ' pet-found" id="pet-log-img-',
-            ).replace(
-                'if(!PETS[i].ignoreCompletion){$("#petlog-container").append(\'<img class="skill-icon-md" id="pet-log-img-\'+i+\'" src="\'+CDNDIR+\'assets/media/main/question.svg">\');tooltip="<div class=\\"text-center\\">???<br><small class=\'text-danger\'>Hint: "+PETS[i].acquiredBy+"</small></div>";}',
-                '{$(\'#petlog-container\').append(\'<a href="https://wiki.melvoridle.com/index.php?title=\' + PETS[i].name + \'" target="blank"><img class="skill-icon-md" id="pet-log-img-\' + i + \'" style="opacity:0.3;filter: grayscale(50%);" src="\' + PETS[i].media + \'"></a>\');tooltip = \'<div class="text-center">\' + PETS[i].name + \'<br><small class="text-danger">Hint: \' + PETS[i].acquiredBy + \'</small></div>\';}',
-            )
-        );
+        // build Monster and pet logs
+        buildMonsterLog();
+        buildPetLog();
 
         // logging
         console.log('Melvor Completion Log Helper Loaded');
