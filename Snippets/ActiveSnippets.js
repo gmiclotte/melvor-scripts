ce5d879361c4f67123e084448abca87c25c6b0f6
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-02-01 10:47:03 +0100

    Snippets: v0.0.13

--- Snippets/ActiveSnippets.js
+++ Snippets/ActiveSnippets.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Melvor Snippets
 // @namespace	http://tampermonkey.net/
-// @version		0.0.12
+// @version		0.0.13
 // @description	Collection of various snippets
 // @grant		none
 // @author		GMiclotte
@@ -146,6 +146,24 @@ getLocalisationKey = (text) => {
     }
     return list;
 }
+snippet.end();
+
+//////////////////////
+//ListRaidUnlocks.js//
+//////////////////////
+snippet.name = 'ListRaidUnlocks.js';
+snippet.start();
+// list unlocked raid items
+listCrateItems = (unlocked = true) =>
+    RaidManager.crateItemWeights.filter(x =>
+        unlocked === game.golbinRaid.ownedCrateItems.has(x.itemID)
+    ).forEach(x =>
+        console.log(items[x.itemID].name)
+    );
+// to list the ones you have unlocked:
+// listCrateItems()
+// to list the ones you haven't unlocked:
+// listCrateItems(false)
 snippet.end();
 
 //////////////////
