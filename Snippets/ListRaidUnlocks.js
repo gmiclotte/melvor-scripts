14e4474a3f97c186f3ca2c3b703a175345e578a8
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-02-01 10:46:48 +0100

    Snippets: List Raid Unlocks

---
+++ Snippets/ListRaidUnlocks.js
@@ -0,0 +1,11 @@
+// list unlocked raid items
+listCrateItems = (unlocked = true) =>
+    RaidManager.crateItemWeights.filter(x =>
+        unlocked === game.golbinRaid.ownedCrateItems.has(x.itemID)
+    ).forEach(x =>
+        console.log(items[x.itemID].name)
+    );
+// to list the ones you have unlocked:
+// listCrateItems()
+// to list the ones you haven't unlocked:
+// listCrateItems(false)
