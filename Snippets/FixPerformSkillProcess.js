30dc5d9c79aa4b737174130907b86e852d2fa21d
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-03-05 23:41:23 +0100

    Snippets: remove FixPerformSkillProcess

--- Snippets/FixPerformSkillProcess.js
+++
@@ -1,8 +0,0 @@
-// fix perform skill process
-eval(performSkillProcess.toString().replace(
-    'if(!confirmedAdded&&!offline)return false;',
-    'if (!confirmedAdded && !ignoreBankFull && !offline) return false;'
-).replace(
-    /^function (\w+)/,
-    'window.$1 = function'
-));
