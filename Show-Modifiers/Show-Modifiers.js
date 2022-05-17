a1fae46e9f472c29221f15cb1543249bfcd73c72
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-05-17 12:01:56 +0200

    Show Modifiers: v0.2.13

--- Show-Modifiers/Show-Modifiers.js
+++ Show-Modifiers/Show-Modifiers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Melvor Show Modifiers
 // @namespace   http://tampermonkey.net/
-// @version     0.2.12
+// @version     0.2.13
 // @description Adds a button to show all your modifiers
 // @author		GMiclotte
 // @include		https://melvoridle.com/*
