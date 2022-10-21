e0e9454c8d69e34bd9b7080a2bb7a9fb4f745cbf
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-10-22 00:45:02 +0200

    Unlimited Offline: update build tools

--- Unlimited-Offline/build.js
+++ Unlimited-Offline/build.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const path = require('path');
 
 // dirs
-copydir.sync('icons', 'dist/icons');
+copydir.sync('icons', 'build/icons');
 // js files
-fs.copyFileSync('setup.js', 'dist/setup.js');
-fs.copyFileSync('manifest.json', 'dist/manifest.json');
+fs.copyFileSync('setup.js', 'build/setup.js');
+fs.copyFileSync('manifest.json', 'build/manifest.json');
 
 const { version } = require('./package.json');
 // copy manifest.json
 const manifest = JSON.parse(fs.readFileSync('manifest.json').toString());
 manifest.version = version
-fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest, null, 4));
+fs.writeFileSync('build/manifest.json', JSON.stringify(manifest, null, 4));
 
 const { namespace } = require('./manifest.json');
-const zipName = path.join(__dirname, '.build', `${namespace}-v${version}.zip`);
+const zipName = path.join(__dirname, 'dist', `${namespace}-v${version}.zip`);
 console.log(zipName)
 fs.mkdirSync(path.dirname(zipName), { recursive: true });
 
@@ -24,7 +24,7 @@ const output = fs.createWriteStream(zipName);
 const archive = archiver('zip');
 archive.pipe(output);
 
-archive.directory('dist', '');
+archive.directory('build', '');
 
 if (require.main === module) {
     archive.finalize();
--- Unlimited-Offline/setup.js
+++ Unlimited-Offline/setup.js
@@ -1,9 +1,7 @@
 export function setup({patch}) {
-    patch(Game, 'getOfflineTimeDiff').replace(() => {
-        const currentTime = Date.now();
-        const originalTimeDiff = currentTime - game.tickTimestamp;
-        const timeDiff = originalTimeDiff;
-        return {timeDiff, originalTimeDiff};
+    patch(Game, 'getOfflineTimeDiff').replace(function() {
+        const originalTimeDiff = Date.now() - this.tickTimestamp;
+        return {timeDiff: originalTimeDiff, originalTimeDiff: originalTimeDiff};
     });
     // patch the max offline time message out
     loadedLangJson.MENU_TEXT.MAX_OFFLINE_TIME = ' ';
