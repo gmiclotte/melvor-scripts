95a1d38a5eac052da8f04e2f6facd3e7fc74541f
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-10-21 21:55:54 +0200

    ETA: load style sheet

--- ETA/src/setup.ts
+++ ETA/src/setup.ts
@@ -1,6 +1,9 @@
 import {ETA} from './ETA';
 
 export function setup(ctx: any): void {
+    // load style sheet
+    ctx.loadStylesheet('styles/tinyMod.css');
+    // create ETA object
     ctx.onInterfaceReady((_: any) => {
         ctx.api({
             // @ts-ignore
